dc3eeba675776bcbdc029ff56d1c3d5865a52617
bhartisatyendra <bhartisatyendra44@gmail.com>
2023-12-05 23:47:02 +0530

    Add CBSE paper model using HTML

---
+++ Javascript/Variable.js
@@ -0,0 +1,36 @@
+
+
+// In javascript we can not define the type of variable because it is a dynamically type language which is detect the of variable
+// at run time
+
+// we can not define variable normally in javascipt some keyword are used to define the variable use such as let ,const and var
+// console.log("HelLo Javascipt")
+// fullname = "Satyendra Bharti";
+// age = 20;
+// price = 344;
+// x = null;
+// y = undefined;
+// isFollow = true;
+// isFollow = false;
+// console.log(isFollow);
+
+let fullName = "Satyendra Bharti"; // we follow to write variable name use camelCase ..
+// let age = 20;
+// let totalPrice = 5000;
+// var age = 34;
+// var age = 67;
+const totalPrice = 4500;
+// console.log(to)
+// console.log(age);
+// console.log(totalPrice);
+// console.log(fullName);
+
+const studentDetail = {
+    name: "Satyendra",
+    age: 24,
+    marks: 400,
+    isPass: true
+
+};
+console.log(studentDetail);
+console.log
--- Javascript/script.js
+++ Javascript/script.js
@@ -1,1 +1,2 @@
 window.alert("Hello");
+console.log("Hello Javascript");
